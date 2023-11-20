fae267baa0d7aae65cb7fb3829c3f2468bba52b7
Yosuke Horie <Yosuke.Horie@sony.com>
2023-11-21 08:07:48 +0900

    Refactor

--- sketch_scd40/sketch_scd40.ino
+++ sketch_scd40/sketch_scd40.ino
@@ -20,22 +20,89 @@ const float kThresholdCO2 = 1000.0f;
 const float kThresholdAHLow = 7.0f;
 const float kThresholdAHMid = 11.0f;
 
-struct SensingInformation {
-  float temperature;
-  uint16_t co2;
-  float relative_humidity;
-  float absolute_humidity;
+class SCD40 {
+ public:
+  SCD40() {}
+  ~SCD40() {}
+  void Initialize() {
+    // Initialize I2C
+    M5.Axp.setBoostBusOutEn(true);
+    Wire.begin(2, 1);
+
+    // wait until sensors are ready, > 1000 ms according to datasheet
+    delay(kInitialTimeMilliSec);
+
+    // start scd measurement in periodic mode, will update every 5 s
+    Wire.beginTransmission(kScdAddress);
+    Wire.write(0x21);
+    Wire.write(0xb1);
+    Wire.endTransmission();
+
+    // wait for first measurement to be finished
+    delay(kIntervalTimeMilliSec);
+  }
+
+  struct SensingInformation {
+    float temperature;
+    uint16_t co2;
+    float relative_humidity;
+    float absolute_humidity;
+  };
+  void GetSensingInformation(SensingInformation & si) {
+    // send read data command
+    Wire.beginTransmission(kScdAddress);
+    Wire.write(0xec);
+    Wire.write(0x05);
+    Wire.endTransmission();
+
+    // read measurement data: 2 bytes co2, 1 byte CRC,
+    // 2 bytes T, 1 byte CRC, 2 bytes RH, 1 byte CRC,
+    // 2 bytes sensor status, 1 byte CRC
+    // stop reading after 12 bytes (not used)
+    // other data like  ASC not included
+    const auto kDataNum = 12;
+    Wire.requestFrom(kScdAddress, kDataNum);
+    auto counter = 0;
+    uint8_t data[kDataNum];
+    while (Wire.available()) {
+      data[counter++] = Wire.read();
+    }
+    uint16_t word[3];
+    word[0] = Byte2Word(data[0], data[1]);
+    word[1] = Byte2Word(data[3], data[4]);
+    word[2] = Byte2Word(data[6], data[7]);
+
+    si.co2 = word[0];
+    si.temperature = CalcTemperature(word[1]);
+    si.relative_humidity = CalcRelativeHumidity(word[2]);
+    si.absolute_humidity = CalcAbsoluteHumidity(si.temperature, si.relative_humidity);
+  }
+
+ private:
+  inline uint16_t Byte2Word(uint8_t msb, uint8_t lsb) {
+    return (static_cast<uint16_t>(msb) << 8) | lsb;
+  }
+
+  float CalcTemperature(uint16_t data) {
+    auto temperature =
+        -45 + 175 * static_cast<float>(data) / 65536;
+    return temperature;
+  }
+
+  float CalcRelativeHumidity(uint16_t data) {
+    auto relative_humidity = static_cast<float>(data) / 65536;
+    return relative_humidity;
+  }
+
+  float CalcAbsoluteHumidity(float temperature, float relative_humidity) {
+    auto e = 6.1078 * std::pow(10, (7.5 * temperature / (temperature + 237.3)));
+    auto a = (217 * e) / (temperature + 273.15);
+    auto absolute_humidity = a * relative_humidity;
+    return absolute_humidity;
+  }
 };
 
-inline uint16_t Byte2Word(uint8_t msb, uint8_t lsb) {
-  return (static_cast<uint16_t>(msb) << 8) | lsb;
-}
-
-float CalcTemperature(uint16_t data);
-float CalcRelativeHumidity(uint16_t data);
-float CalcAbsoluteHumidity(float temperature, float relative_humidity);
-void Display(const SensingInformation &si);
-
+SCD40 scd40_;
 void setup() {
   M5.begin(true, true, false);
 
@@ -44,77 +111,17 @@ void setup() {
   M5.Lcd.setTextColor(kTextColor, kBackgroundColor);
   M5.Lcd.setTextSize(kTextSize);
 
-  // Initialize I2C
-  M5.Axp.setBoostBusOutEn(true);
-  Wire.begin(2, 1);
-
-  // wait until sensors are ready, > 1000 ms according to datasheet
-  delay(kInitialTimeMilliSec);
-
-  // start scd measurement in periodic mode, will update every 5 s
-  Wire.beginTransmission(kScdAddress);
-  Wire.write(0x21);
-  Wire.write(0xb1);
-  Wire.endTransmission();
-
-  // wait for first measurement to be finished
-  delay(kIntervalTimeMilliSec);
+  scd40_.Initialize();
 }
 
 void loop() {
-  // send read data command
-  Wire.beginTransmission(kScdAddress);
-  Wire.write(0xec);
-  Wire.write(0x05);
-  Wire.endTransmission();
-
-  // read measurement data: 2 bytes co2, 1 byte CRC,
-  // 2 bytes T, 1 byte CRC, 2 bytes RH, 1 byte CRC,
-  // 2 bytes sensor status, 1 byte CRC
-  // stop reading after 12 bytes (not used)
-  // other data like  ASC not included
-  const auto kDataNum = 12;
-  Wire.requestFrom(kScdAddress, kDataNum);
-  auto counter = 0;
-  uint8_t data[kDataNum];
-  while (Wire.available()) {
-    data[counter++] = Wire.read();
-  }
-  uint16_t word[3];
-  word[0] = Byte2Word(data[0], data[1]);
-  word[1] = Byte2Word(data[3], data[4]);
-  word[2] = Byte2Word(data[6], data[7]);
-
-  SensingInformation si;
-  si.co2 = word[0];
-  si.temperature = CalcTemperature(word[1]);
-  si.relative_humidity = CalcRelativeHumidity(word[2]);
-  si.absolute_humidity = CalcAbsoluteHumidity(si.temperature, si.relative_humidity);
-
+  SCD40::SensingInformation si;
+  scd40_.GetSensingInformation(si);
   Display(si);
-
   delay(kIntervalTimeMilliSec);
 }
 
-float CalcTemperature(uint16_t data) {
-  auto temperature =
-      -45 + 175 * static_cast<float>(data) / 65536;
-  return temperature;
-}
-
-float CalcRelativeHumidity(uint16_t data) {
-  auto relative_humidity = static_cast<float>(data) / 65536;
-  return relative_humidity;
-}
-
-float CalcAbsoluteHumidity(float temperature, float relative_humidity) {
-  auto e = 6.1078 * std::pow(10, (7.5 * temperature / (temperature + 237.3)));
-  auto a = (217 * e) / (temperature + 273.15);
-  auto absolute_humidity = a * relative_humidity;
-  return absolute_humidity;
-}
-
-void Display(const SensingInformation &si) {
+void Display(const SCD40::SensingInformation &si) {
   M5.Lcd.setTextColor(kTextColor, kBackgroundColor);
   int x = kDisplayOffsetX;
   int y = kDisplayOffsetY;
